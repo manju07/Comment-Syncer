ac83afb896b6d1adc9d962dd36de675296875612
Manjunath <itzmemanju16@gmail.com>
2018-02-22 11:47:51 +0530

    working fine

--- App/Controller/webControllerApis.js
+++ App/Controller/webControllerApis.js
@@ -1,3 +1,6 @@
+var DiffMatchPatch = require('diff-match-patch');
+var dmp = new DiffMatchPatch();
+
 var mongoose = require('mongoose');
 
 //Set up default mongoose connection
@@ -17,17 +20,22 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // Modules
 let comment = require("./../Modeles/commentModule");
 
+let synComment="";
+let fetchComment="";
+
 let webPagesCtrl = function () {
     this.sync = function (req, res) {
         let options = { upsert: true, new: true, setDefaultsOnInsert: true };
+        synComment=req.body.comment;
+        let result=dmp.diff_main(fetchComment, synComment) ;
+        dmp.diff_cleanupSemantic(result);
+        console.log("diff_main result:"+result);
         comment.findOneAndUpdate({ name: "manju" }, { comment: req.body.comment }, options, function (err, result) {
             if (err) {
-                console.log("err", err);
                 res.send({ status: fail, message: "fail" });
                 res.end();
             }
             else {
-                console.log("result", result);
                 res.send({ status: true, message: "success" });
                 res.end();
             }
@@ -38,12 +46,12 @@ let webPagesCtrl = function () {
     this.fetch = function (req, res) {
         comment.find({ name: "manju" }, function (err, result) {
             if (err) {
-                console.log("err", err);
+                
                 res.send({ status: fail, result: "fail" });
                 res.end();
             }
             else {
-                console.log("result", result);
+                fetchComment=result[0].comment;
                 res.send({ status: true, "comment": result[0].comment });
                 res.end();
             }
--- Public/js/commentsyncer.js
+++ Public/js/commentsyncer.js
@@ -1,39 +1,58 @@
+app.controller('commentSync', ['$scope', '$http', function ($scope, $http) {
+  let t1 = "";
+  let t2 = "";
+  $scope.comment = "";
 
-console.log("hi hello");
-app.controller('commentSync',['$scope','$http',function($scope,$http){
-  
-  $scope.comment="";
-  console.log("inside commentSync")
-  $scope.fetchComment = function(req,res){
-    
+  $scope.fetchComment = function (req, res) {
+
     console.log("calling fetch comment method");
     $http({
-                method  : 'GET',
-                url     : __appurl + "comment/fetch",
-                data    :{},
-                headers : { 'Content-Type': 'application/json' }  
-    }).then(function (success){
-      console.log("success"+JSON.stringify(success.data.comment));
-      $scope.comment=success.data.comment;
-    },function (error){
-      console.log("error"+JSON.stringify(error));      
+      method: 'GET',
+      url: __appurl + "comment/fetch",
+      data: {},
+      headers: { 'Content-Type': 'application/json' }
+    }).then(function (success) {
+      console.log("success" + JSON.stringify(success.data.comment));
+      $scope.comment = success.data.comment;
+      t1 = success.data.comment;
+    }, function (error) {
+      console.log("error" + JSON.stringify(error));
     });
-    
-  }
-  $scope.updateComment = function(req,res){
-    
+
+  };
+
+  $scope.updateComment = function (req, res) {
+    t2 = $scope.comment;
     console.log("calling update comment method");
-    console.log($scope.comment);
-    $http({
-                method  : 'POST',
-                url     : __appurl + "comment/sync",
-                data    : {comment:$scope.comment},
-                headers : { 'Content-Type': 'application/json' }  
-            }).then(function (success){
-              console.log("success:"+JSON.stringify(success));
-            },function (error){
-              console.log("error:"+error);
-            });
     
-  }
+    var dmp = new diff_match_patch();
+    let result = dmp.diff_main(t1, t2);
+    dmp.diff_cleanupSemantic(result);
+
+    console.log("diff_main result:" + result);
+
+   let tempComment="";
+    for(let i=0;i<result.length;i++)
+    {
+      if(result[i][0]>=0)
+      {
+        tempComment+=result[i][1];
+      }
+    }
+    console.log("tempComment:"+tempComment);
+
+    $http({
+      method: 'POST',
+      url: __appurl + "comment/sync",
+      data: { comment: $scope.comment },
+      headers: { 'Content-Type': 'application/json' }
+    }).then(function (success) {
+
+      console.log("success:" + JSON.stringify(success));
+    }, function (error) {
+      console.log("error:" + error);
+    });
+
+  };
+
 }]);
