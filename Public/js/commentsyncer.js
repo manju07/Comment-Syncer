156a7523dd75c1fb88456efc478a6d228621d7db
Manjunath <itzmemanju16@gmail.com>
2018-02-23 09:24:44 +0530

    changes in commentsyncer.js

--- Public/js/commentsyncer.js
+++ Public/js/commentsyncer.js
@@ -21,31 +21,28 @@ app.controller('commentSync', ['$scope', '$http', function ($scope, $http) {
 
   };
 
-  $scope.updateComment = function (req, res) 
-  {
+  $scope.updateComment = function (req, res) {
     SyncComment = $scope.comment;
     console.log("calling update comment method");
-    
+
     var dmp = new diff_match_patch();
     let result = dmp.diff_main(fetchComment, SyncComment);
     dmp.diff_cleanupSemantic(result);
 
     console.log("diff_main result:" + result);
 
-   let tempComment="";
-    for(let i=0;i<result.length;i++)
-    {
-      if(result[i][0]>=0)
-      {
-        tempComment+=result[i][1];
+    let tempComment = "";
+    for (let i = 0; i < result.length; i++) {
+      if (result[i][0] >= 0) {
+        tempComment += result[i][1];
       }
     }
-    console.log("tempComment:"+tempComment);
+    console.log("tempComment:" + tempComment);
 
     $http({
       method: 'POST',
       url: __appurl + "comment/sync",
-      data: { comment: tempComment},
+      data: { comment: tempComment },
       headers: { 'Content-Type': 'application/json' }
     }).then(function (success) {
       fetchComment = tempComment;
